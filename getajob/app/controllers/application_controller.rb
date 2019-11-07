f624a3cced7d043f36476d02a988281cba3c0b92
Richard N <rickky8555@hotmail.com>
2019-11-07 18:05:26 +0000

    dashboard links redirect depending on admin

--- getajob/app/controllers/application_controller.rb
+++ getajob/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  def admin_user?
+  current_user && current_user.admin?    
+  # assuming admin is the name of your boolean field.  
+end
 end
--- getajob/db/migrate/20191017191924_create_jobrecs.rb
+++ getajob/db/migrate/20191017191924_create_jobrecs.rb
@@ -4,8 +4,7 @@ class CreateJobrecs < ActiveRecord::Migration[5.0]
       t.string :first_name
       t.string :last_name
       t.string :email_address
-      t.string :associate_company
-#associate company, date of account creation, role, admin role, 
+
       t.timestamps
     end
   end
