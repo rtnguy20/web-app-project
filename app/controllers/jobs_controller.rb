376c1f29cdfc3500e938817555c77d465a29e08e
Richard N <rickky8555@hotmail.com>
2019-11-21 14:26:49 +0000

    commented out another line

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < 
-  before_action :set_job, only: [:show, :edit, :update, :destroy]
+  #before_action :set_job, only: [:show, :edit, :update, :destroy]
   #before_action :authenticate_user!, :except => [:index]
 #...
 
